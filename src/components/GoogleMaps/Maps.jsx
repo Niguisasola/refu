9e34e49899b75832219463e6392ab5b3782fb101
Nika <nikaguisasola@gmail.com>
2022-09-30 00:11:20 +0200

    done

--- src/components/GoogleMaps/Maps.jsx
+++ src/components/GoogleMaps/Maps.jsx
@@ -5,25 +5,24 @@ import GoogleMaps from "simple-react-google-maps";
 
 
 export default function Mapa({ markers }) {
-    
+
 
     return (
         <> 
-        <div className='mapgoogle d-inline-flex justify-content-center'>
-            <div className='mt-2 mb-3'>
-                        
-        <GoogleMaps
-                    apiKey={"AIzaSyCnMxDM5mzZuwHEsYeb1b_CcA1Pq1nxigE"}
-                    key={1}
-                    style={{ height: "50vh", width: "98vw", margin:"10px"}}
-                    zoom={15}
-                    center={{ lat: 41.449802616705426, lng: 2.2032907306799157 }}
-                    markers={markers} //optional
-                // center={{ lat: 39.4863243, lng: -0.3602353 }}
-                // markers={restrooms.map((restroom) => ({ lat: restroom.latitude, lng: restroom.longitude}))}
-                />
+        
+            <div className='mapgoogle d-flex justify-center'>
+                <div className='relative mt-2 mb-3'>
+                    <GoogleMaps
+                    className=''
+                        apiKey={"AIzaSyCnMxDM5mzZuwHEsYeb1b_CcA1Pq1nxigE"}
+                        key={1}
+                        style={{height: "50vh", width: "98vw", margin: "10px" }}
+                        zoom={15}
+                        center={{ lat: 41.449802616705426, lng: 2.2032907306799157 }}
+                        markers={markers}>
+                    </GoogleMaps>
+                </div>
             </div>
-        </div>
         </>
     );
 }
--- src/components/NavButtons.jsx
+++ src/components/NavButtons.jsx
@@ -11,7 +11,6 @@ function NavButtons() {
         <div className="buttons d-flex justify-content-evenly gap-3 m-3">
             <Link to='/add'><button className='btn btn-white border-dark rounded-4 opacity-75' type="submit"><img className='bg-img' src={add} alt='add' /></button></Link>
             <Link to='/ranking'><button className='btn btn-white border-dark rounded-4 opacity-75' type="submit"><img className='bg-img' src={ranking} alt='ranking' /></button></Link>
-            <Link to='/favorites'><button className='btn btn-white border-dark rounded-4 opacity-75' type="submit"><img className='bg-img' src={favorites} alt='ranking' /></button></Link>
             <Link to='/listed'><button className='btn btn-white border-dark rounded-4 mr-3' type="submit"><img className='bg-img' src={listed} alt='listed' /></button></Link>
         </div>
         </div>
